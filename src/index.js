488a13e11d6cbe0122a62ba0c04255446004e35e
jpuri <jyotipuri@gmail.com>
2015-12-06 19:38:46 +0530

    fix: removing use of componentWillMount method to support isomorphic applications

--- src/index.js
+++ src/index.js
@@ -5,18 +5,19 @@ import Immutable from 'immutable';
 let componentUpdateHooks = new Immutable.Map({});
 let componentUpdateHookCounter=0;
 
-// Collection of history of all operations in the app.
-let opHistory = new Immutable.List();
+// Collection of history of all actions in the app.
+let actionHistory = new Immutable.List();
 
 // The function is used to update app state of composing components.
-export const updateAppState = function(op, key, value) {
-  opHistory = opHistory.push({
-    op: op,
+export const updateAppState = function(actionType, key, value) {
+  const action = {
+    actionType: actionType,
     key: key,
     value: value
-  })
-  componentUpdateHooks.forEach(function(updateHook) {
-    updateHook();
+  };
+  actionHistory = actionHistory.push(action)
+  componentUpdateHooks.forEach(function(updateComponent) {
+    updateComponent(action);
   })
 }
 
@@ -25,56 +26,58 @@ export const updateAppState = function(op, key, value) {
 export var configureStore = (fields) => {
   return (component) => {
     class newComponent extends Component  {
-      // initialize state with an empty store
       constructor(properties) {
         super(properties);
         this.hookIndex = componentUpdateHookCounter;
-        componentUpdateHookCounter++;
-        this.state = { store: new Immutable.Map({}) };
-        // I am not sure how can I get rid of this line :(
         this.MyComponent = React.createFactory(component);
-      }
-      // When component is mount, the opCounter is reset to 0 and update method is added to collection componentUpdateHooks.
-      componentDidMount() {
-        this.opCounter = 0;
+        this.state = {store: this.getInitialStore()};
+        this.actionIndex = 0;
         componentUpdateHooks = componentUpdateHooks.set(this.hookIndex, this.updateComponentStore.bind(this));
-        this.updateComponentStore();
+        componentUpdateHookCounter++;
       }
       // When component is unmount, its entry is removed from collection componentUpdateHooks.
       componentWillUnmount() {
         componentUpdateHooks = componentUpdateHooks.remove(this.hookIndex);
       }
       // The function will update the store.
-      // It will execute all operation on opHistory which are still not executed for this store.
-      updateComponentStore() {
+      // It will execute all action on actionHistory which are still not executed for this store.
+      updateComponentStore(action) {
         let store = this.state.store;
-        for(let i = this.opCounter;i < opHistory.size;i++) {
-          const op = opHistory.get(i);
-          this.opCounter++;
-          if(fields.indexOf('__all__') >= 0 || fields.indexOf(op.key) >= 0) {
-            if(op.op === 'NEW') {
-              // NEW is for new field added to app state
-              store = store.set(op.key, op.value);
-            } else if(op.op === 'REMOVE') {
-              // REMOVE is for field removed from app state
-              store = store.remove(op.key);
-            } else if(op.op === 'APPEND') {
-              // APPEND is for new value added to existing list
-              let list = store.get(op.key);
-              list = list.append(op.value);
-              store = store.set(op.key, list);
-            } else if(op.op === 'DELETE') {
-              // DELETE_KEY is for value to be removed from existing list
-              let list = store.get(op.key);
-              list = list.remove(op.value);
-              store = store.set(op.key, list);
-            }
-          }
-        };
+        store = this.executeAction(store, action);
         this.setState({
           store: store
         });
       }
+      // This function will execute all action on actionHistory which are still not executed for this store.
+      getInitialStore() {
+        let store = new Immutable.Map({});
+        for(let i = this.actionIndex;i < actionHistory.size;i++) {
+          store = this.executeAction(store, actionHistory[i]);
+          this.actionIndex++;
+        }
+        return store;
+      }
+
+      // Execute single action
+      executeAction(store, action) {
+        if(fields.indexOf('__all__') >= 0 || fields.indexOf(action.key) >= 0) {
+          if(action.actionType === 'NEW') {
+            store = store.set(action.key, action.value);
+          } else if(action.actionType === 'REMOVE') {
+            store = store.remove(action.key);
+          } else if(action.actionType === 'APPEND') {
+            let list = store.get(action.key);
+            list = list.append(action.value);
+            store = store.set(action.key, list);
+          } else if(action.actionType === 'DELETE') {
+            let list = store.get(action.key);
+            list = list.remove(action.value);
+            store = store.set(action.key, list);
+          }
+        }
+        return store;
+      }
+
       // rendering component
       render() {
         return (
